6c455a9191cfb4556df938a2352fd0a4637e9b80
Peter Akasi Mba <awintechworld@gmail.com>
2025-09-23 12:20:32 +0000

    url parameters response and update routes handled

--- app.js
+++ app.js
@@ -10,12 +10,38 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// GET TOURS
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
       results: tours.length,
       tours,
+    },
+  });
+});
+
+// GET A SINGLE TOUR
+app.get("/api/v1/tours/:id", (req, res) => {
+  // Get Param ID and convert it to a number
+  const id = req.params.id * 1;
+
+  // Find tour using the ID
+  const tour = tours.find((el) => el.id === id);
+
+  // Handle error when ID is INVALID
+  if (!tour) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Invallid ID",
+    });
+  }
+
+  // SEND TOUR TO CLIENT
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
     },
   });
 });
@@ -45,6 +71,33 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
+});
+
+// UPDATE TOUR
+app.patch("/api/v1/tours/:id", (req, res) => {
+  // Get Param ID and convert it to a number
+  const id = req.params.id * 1;
+
+  // Find tour using the ID
+  const tour = tours.find((el) => el.id === id);
+
+  // Handle ERROR when ID is INVALID
+  if (!tour) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
+  }
+
+  // // Update the tour with new data
+  // Object.assign(tour, req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour...>",
+    },
+  });
 });
 
 app.listen(port, () => {
