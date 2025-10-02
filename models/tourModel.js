4cd57ccf4395e850bf668d114d379e92e1ba4543
Peter Akasi Mba <awintechworld@gmail.com>
2025-10-02 13:46:27 +0000

    modelled the tours api

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,55 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    difficulty: 0,
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    required: [true, "A tour must have a summary"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have an image cover"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
