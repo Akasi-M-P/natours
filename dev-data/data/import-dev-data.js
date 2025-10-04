397d34109cac12e4fc22e322c476bcdc3de44ebe
Peter Akasi Mba <awintechworld@gmail.com>
2025-10-04 21:32:14 +0000

    implement a script to import and delete data in the host database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("./../../models/tourModel");
+
+dotenv.config({ path: "./config.env" });
+
+// CONNECT HOSTED MONGODB TO APP USING MONGOOSE
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => console.log(con.connections))
+  .then(() => console.log("DB Connected Successfully"))
+  .catch((err) => console.log(err));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+// IMPORT DATE INTO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Data successfully loaded");
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DATABASE
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted");
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
+
+console.log(process.argv);
