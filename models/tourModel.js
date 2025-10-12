dd7bfd7901ac441a2072c1c47bbde5e1141362b1
Peter Akasi Mba <awintechworld@gmail.com>
2025-10-12 22:39:56 +0000

    implemented the mongoose document middleware that allows data manipulation before a doc is created or saved

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 // THIS CREATES A TOUR MODEL USING MONGOOSE
 const tourSchema = new mongoose.Schema(
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -69,6 +71,22 @@ tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
 
+// MONGOOSE DOC MIDDLEWARE: THE PRE MIDDLEWARE RUN BEFORE A DOC IS "SAVE()" OR "CREATE()" ONLY"
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre("save", function (next) {
+//   console.log("DOCUMENT BEING CREATED");
+//   next();
+// });
+
+// // MONGOOSE DOC MIDDLEWARE: THE POST MIDDLEWARE RUN AFTER A DOC IS "SAVE()" OR "CREATE()" ONLY"
+// tourSchema.post("save", function (next) {
+//   console.log("DOCUMENT SAVED FROM THE POST MIDDLEWARE");
+//   // next();
+// });
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
