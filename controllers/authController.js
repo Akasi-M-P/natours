b01512154075ce0b975e9faad3940e5f1bc0e06a
Peter Akasi Mba <awintechworld@gmail.com>
2025-12-24 22:23:48 +0000

    implement jwt for signing up new users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
+//SIGNS UP NEW USER WITH VALID DETAILS
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  //GENERATES UNIQUE TOKEN FOR USER AND DETERMINES WHEN TOKEN EXPIRES
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
