0f3672166b415e53abe4ff7ace8843f4e7208a23
Peter Akasi Mba <awintechworld@gmail.com>
2025-12-29 22:10:16 +0000

    implemented http security headers, data sanitization and preventing parameter pollution

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
 // CORE MODULES
 const express = require("express");
 const morgan = require("morgan");
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-
-const app = express();
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xxs = require("xss-clean");
+const hpp = require("hpp");
 
 // CUSTOM MODULES
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 const tourRoute = require("./routes/tourRoutes");
 const userRoute = require("./routes/userRoutes");
 
-// REGULAR MODULES
+const app = express();
+
+// GLOBAL MODULES
+
+// SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+// DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+// LIMIT REQUESTS FROM SAME API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requets from this IP address, please try again in an hour",
+});
+
+app.use("/api", limiter);
+
+// BODY PARSER, READS DATA FROM BODY INTO REQ.BODY
+app.use(express.json({ limit: "10kb" }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATIO AGAINST XXS
+app.use(xxs());
+
+// PREVENTS PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "maxGroupSize",
+      "difficulty",
+      "ratingsAverage",
+      "price",
+      "ratingQuantity",
+    ],
+  })
+);
 
 // STATIC FILES URL DIRECTORY
 app.use(express.static(`${__dirname}/public`));
