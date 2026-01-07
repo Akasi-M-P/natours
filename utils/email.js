8b6364362a171d56b1b30c542e02e6154e18941e
Peter Akasi Mba <awintechworld@gmail.com>
2026-01-07 00:42:23 +0000

    built an email handler

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,56 @@
 const Nodemailer = require("nodemailer");
+const pug = require("pug");
 
-const sendEmail = async (options) => {
-  // CREATE A TRANSPORTER
-  const transporter = Nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Peter Mba Akasi <${process.env.EMAIL_FROM}>`;
+  }
 
-  // DEFINE THE EMAIL OPTIONS
-  const mailOptions = {
-    from: "Peter Mba Akasi <awintechworld@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // SENDGRID
+      return 1;
+    }
 
-  // SEND EMAIL
-  await transporter.sendMail(mailOptions);
+    return Nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // SEND ACTUAL EMAIL
+  async send(template, subject) {
+    // RENDER HTML BASED ON A PUG TEMPLATE
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // DEFINE EMAIL OPTIONS
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: options.message,
+    };
+
+    // CREATE A TRANSPORT AND SEND EMAIL
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("Welcome", "Welcome to the Natours Family");
+  }
 };
-
-module.exports = sendEmail;
